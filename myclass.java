13a2cf793eb901d2e02b5362c7e11d76ccbce7cb
Siraj17-10 <uli75244@gmail.com>
2026-03-31 12:47:42 +0530

    Add 'Good Morning' message to myclass

--- myclass.java
+++ myclass.java
@@ -4,5 +4,7 @@ public class myclass {
 	   public static void main(String[] args) {
 	        
 	        System.out.println("Hello, World!");
+		    System.out.println("Good Morning");
+		   
  }
 }
